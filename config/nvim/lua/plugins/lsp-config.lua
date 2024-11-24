8b75e51b8caa3ae6ae90bcfef68c57a2a2318b72
Nadavushka <nadavushkadev@gmail.com>
2024-11-24 17:57:18 +0200

    fix leader missing

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -79,10 +79,10 @@ return {
       })
 
       vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-      vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
-      vim.keymap.set("n", "en", vim.diagnostic.goto_next)
-      vim.keymap.set("n", "ep", vim.diagnostic.goto_prev)
+      vim.keymap.set("n", "<leader>en", vim.diagnostic.goto_next)
+      vim.keymap.set("n", "<leader>ep", vim.diagnostic.goto_prev)
 
       vim.diagnostic.config({
         virtual_text = false
