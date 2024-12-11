4074bc521c474c6abefb162e18dc0cf71c03294a
Nadavushka <nadavushkadev@gmail.com>
2024-12-11 09:52:28 +0200

    ts-ls instead of tsserver

--- config/nvim/lua/plugins/lsp-config.lua
+++ config/nvim/lua/plugins/lsp-config.lua
@@ -9,7 +9,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "tsserver", "html", "pyright", "pylsp", "gopls", "bashls", "jsonls", "cssls", "taplo" }
+        ensure_installed = { "lua_ls", "ts_ls", "html", "pyright", "pylsp", "gopls", "bashls", "jsonls", "cssls", "taplo" }
       })
     end
   },
@@ -34,7 +34,7 @@ return {
         capabilities = capabilities
       })
 
-      lspconfig.tsserver.setup({
+      lspconfig.ts_ls.setup({
         capabilities = capabilities
       })
 
