228fee90dbe73ee823e62f60c379c61d89c18669
Nadavushka <nadavushkadev@gmail.com>
2024-12-11 09:52:58 +0200

    disable arrows in normal mode

--- config/nvim/lua/vim-options.lua
+++ config/nvim/lua/vim-options.lua
@@ -1,9 +1,6 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.scrolloff = 4
-
-vim.opt.undofile = true
-vim.opt.undodir = '.vim/undo'
+vim.opt.scrolloff = 8
 
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
@@ -20,6 +17,19 @@ vim.keymap.set("n", "<c-h>", ":wincmd h<CR>")
 vim.keymap.set("n", "<c-l>", ":wincmd l<CR>")
 vim.keymap.set("n", "<c-k>", ":wincmd k<CR>")
 vim.keymap.set("n", "<c-j>", ":wincmd j<CR>")
+
+vim.api.nvim_set_keymap('n', '<Up>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Down>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Left>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<Right>', '', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('i', '<Up>', '', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('i', '<Down>', '', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('i', '<Left>', '', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('i', '<Right>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Up>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Down>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Left>', '', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('v', '<Right>', '', { noremap = true, silent = true })
 
 vim.keymap.set("n", "<leader>n", ":Neotree filesystem toggle left<CR>")
 
