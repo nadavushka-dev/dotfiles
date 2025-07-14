630e37a7fbdd4a183c8624bcb739601211538ab2
nadavushka-dev <nadavushkadev@gmail.com>
2025-07-14 15:19:36 +0300

    go error checker added

--- config/nvim/lua/config/keybindings.lua
+++ config/nvim/lua/config/keybindings.lua
@@ -18,3 +18,5 @@ vim.api.nvim_set_keymap('n', '<Up>', '', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Down>', '', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Left>', '', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Right>', '', { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>err", "o if err != nil {\n}<Esc>O")
