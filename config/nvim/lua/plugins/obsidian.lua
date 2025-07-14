804293cd3e507c6a5a3eef4606e490fac2a339ee
nadavushka-dev <nadavushkadev@gmail.com>
2025-07-14 15:21:47 +0300

    new plugs

---
+++ config/nvim/lua/plugins/obsidian.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- Always use latest stable release
+    lazy = true,
+    event = { "BufReadPre " .. vim.fn.expand("~") .. "/Library/Mobile Documents/iCloud~md~obsidian/Documents/ObsidianVault/**.md" },
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim",
+    },
+    opts = {
+      workspaces = {
+        {
+          name = "ObsidianVault",
+          path = "~/Library/Mobile Documents/iCloud~md~obsidian/Documents/ObsidianVault",
+        },
+      },
+      ui = { enable = false }
+    },
+  },
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.nvim' }, -- if you use the mini.nvim suite
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovski/mini.icons' }, -- if you use standalone mini plugins
+    -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+    ---@module 'render-markdown'
+    ---@type render.md.UserConfig
+    opts = {},
+  }
+}
