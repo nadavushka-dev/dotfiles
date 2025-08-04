a70c5de06cc3785bf9015b1834531dcdfcca5717
nadavushka-dev <nadavushkadev@gmail.com>
2025-08-04 08:50:57 +0300

    prompt color change

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,7 +3,11 @@ function parse_git_branch() {
 }
 
 COLOR_DEF=$'%f'
-COLOR_DIR=$'%F{197}'
-COLOR_GIT=$'%F{39}'
+COLOR_DIR=$'%F{214}'      # Gruvbox yellow
+COLOR_GIT=$'%F{109}'      # Gruvbox muted blue  
+COLOR_USER=$'%F{175}'     # Gruvbox purple
+COLOR_SEP=$'%F{246}'      # Gruvbox gray
+COLOR_PROMPT=$'%F{108}'   # Gruvbox green
+
 setopt PROMPT_SUBST
-export PROMPT='${COLOR_DIR}%~ >${COLOR_GIT} $(parse_git_branch)${COLOR_DEF}'
+export PROMPT='${COLOR_USER}%n${COLOR_SEP}@${COLOR_DIR}%~ ${COLOR_PROMPT}>${COLOR_GIT} $(parse_git_branch)${COLOR_DEF}'
