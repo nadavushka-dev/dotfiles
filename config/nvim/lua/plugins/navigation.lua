a23b6133f3b89f9194bc70ff8011b77b7b3ed755
nadavushka-dev <nadavushkadev@gmail.com>
2025-08-04 08:51:46 +0300

    diagnostics with tele

--- config/nvim/lua/plugins/navigation.lua
+++ config/nvim/lua/plugins/navigation.lua
@@ -59,6 +59,7 @@ return {
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
       vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+      vim.keymap.set('n', '<leader>fe', builtin.diagnostics, { desc = 'Telescope diagnostics tags' })
     end
   },
   -- HARPOON
