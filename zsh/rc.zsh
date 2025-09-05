a8f492dab0e01e1ff0866d71974b7551d89c8805
nadavushka-dev <nadavushkadev@gmail.com>
2025-09-05 15:53:57 +0300

    zoxide setup is getting its own file

--- zsh/rc.zsh
+++ zsh/rc.zsh
@@ -10,12 +10,8 @@ source_if_exists $DOTFILES/zsh/aliases.zsh
 source_if_exists $DOTFILES/zsh/prompt.zsh
 source_if_exists $DOTFILES/zsh/env.zsh
 source_if_exists $DOTFILES/zsh/options.zsh
+source_if_exists $DOTFILES/zsh/zoxide-setup.zsh
 source_if_exists $DOTFILES/zsh/profile.zsh
-
-. "$HOME/.local/bin/env"
-
-# bun completions
-[ -s "/Users/Nadavushka/.bun/_bun" ] && source "/Users/Nadavushka/.bun/_bun"
 
 fortune fortunes | cowsay
 
