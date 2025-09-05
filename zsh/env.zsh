afb0c4ffc2d5ad066ca1c690a5ecf4a4d84407d5
nadavushka-dev <nadavushkadev@gmail.com>
2025-09-05 15:53:20 +0300

    remove unused export, added sercrets

--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,9 +1,9 @@
 . "$HOME/.cargo/env"
-export AVANTE_BACKEND="ollama"
-export AVANTE_OLLAMA_HOST="http://localhost:11434"
-export AVANTE_OLLAMA_MODEL="llama3.2" 
-export STARSHIP_CONFIG=~/.config/starship/starship.toml
 
 export XDG_CONFIG_HOME="$HOME/.config"
 export XDG_DATA_HOME="$HOME/.local/share"
 export XDG_CACHE_HOME="$HOME/.cache"
+
+. "$HOME/.local/bin/env"
+
+[ -f "$HOME/.config/secrets/ai_keys.env" ] && export $(grep -vE '^\s*#' "$HOME/.config/secrets/ai_keys.env" | xargs)
