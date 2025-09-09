5d48e9ff4444e8d7c97114039bddd9da32e045c8
nadavushka-dev <nadavushkadev@gmail.com>
2025-09-09 11:43:13 +0300

    using better lua syntax

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,37 +1,40 @@
 local wezterm = require("wezterm")
+local config = wezterm.config_builder()
 
-return {
-	bidi_enabled = true,
+config.bidi_enabled = true
 
-	-- Font with better spacing options
-	font = wezterm.font_with_fallback({
-		"JetBrains Mono",
-		"SF Mono", -- Often better Hebrew spacing on macOS
-	}),
-	font_size = 16,
+-- Font with better spacing options
+config.font = wezterm.font_with_fallback({
+	"JetBrains Mono",
+	"SF Mono", -- Often better Hebrew spacing on macOS
+})
+config.font_size = 16
 
-	-- Font rendering options
-	freetype_load_target = "Normal",
-	freetype_render_target = "Normal",
+-- Font rendering options
+config.freetype_load_target = "Normal"
+config.freetype_render_target = "Normal"
 
-	-- Remove harfbuzz features that might affect spacing
-	-- harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+-- Try adjusting cell width
+config.cell_width = 1.0
+config.line_height = 1.0
 
-	-- Try adjusting cell width
-	cell_width = 1.0,
-	line_height = 1.0,
+config.color_scheme = "Gruvbox Material (Gogh)"
+config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
 
-	color_scheme = "Gruvbox Material (Gogh)",
-	window_decorations = "RESIZE",
-	hide_tab_bar_if_only_one_tab = true,
-
-	keys = {
-		{
-			key = "l",
-			mods = "CMD",
-			action = wezterm.action.TogglePaneZoomState,
-		},
+config.keys = {
+	{
+		key = "l",
+		mods = "CMD",
+		action = wezterm.action.TogglePaneZoomState,
 	},
-
-	use_ime = true,
+	{
+		key = "w",
+		mods = "CMD",
+		action = wezterm.action.CloseCurrentTab({ confirm = false }),
+	},
 }
+
+config.use_ime = true
+
+return config
